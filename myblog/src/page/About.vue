5888e70d090910f49a6d15887efe8666922915ac
laifeng <15989103001@163.com>
2018-04-20 23:32:30 +0800

    del

--- myblog/src/page/About.vue
+++ myblog/src/page/About.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
       <h1> This is about </h1>
-       <a href="http://service.weibo.com/share/share.php?appkey=&title=&url=&searchPic=false&style=simple" target="_blank"><img src="/static/weibo.png" alt="wechat"> 分享到微博</a>
      </div>
 </template>
 
